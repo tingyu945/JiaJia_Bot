5fa1cb8161e3a23b80f829762d232130c7de82f3
tingyu945 <54214829+tingyu945@users.noreply.github.com>
2019-08-18 02:51:27 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+import json
+import random
+
+with open('setting.json', 'r', encoding = 'utf8') as jfile:
+    jdata = json.load(jfile)
 
 bot = commands.Bot(command_prefix = '[')
 
@@ -9,16 +14,27 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(612336888694571180)
+    channel = bot.get_channel(int(jdata['Welcome_channel']))
     await channel.send(F'{member} join!')
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(612336911347875841)
+    channel = bot.get_channel(int(jdata['Leave_channel']))
     await channel.send(F'{member} leave!')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(F'{round(bot.latency * 1000)} (ms)')
 
-bot.run("NjEyMzE1MDkzNjM1NDMyNTgw.XVg6tw.kwNywa98C5uNhFQNIk-pmfL8Rn8")
+@bot.command()
+async def pic(ctx):
+    random_pic = random.choice(jdata['pic'])
+    pic = discord.File(random_pic)
+    await ctx.send(file = pic)
+
+@bot.command()
+async def webpic(ctx):
+    random_pic = random.choice(jdata['url_pic'])
+    await ctx.send(random_pic)
+
+bot.run(jdata['TOKEN'])
